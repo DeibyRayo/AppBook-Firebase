aacea324beacd04fccb5bcbd1681fdb39b942a5b
DeibyRayo <deibyrayo95@gmail.com>
2022-05-05 22:04:37 -0500

    login-register-checkEmail

--- app/src/main/java/dev/deiby/esteban/appbook_firebase/CheckEmailActivity.kt
+++ app/src/main/java/dev/deiby/esteban/appbook_firebase/CheckEmailActivity.kt
@@ -1,11 +1,82 @@
 package dev.deiby.esteban.appbook_firebase
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.auth.ktx.userProfileChangeRequest
+import com.google.firebase.ktx.Firebase
+import dev.deiby.esteban.appbook_firebase.databinding.ActivityCheckEmailBinding
 
 class CheckEmailActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivityCheckEmailBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_check_email)
+
+        binding = ActivityCheckEmailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        auth = Firebase.auth
+
+        val user = auth.currentUser
+
+        binding.veficateEmailAppCompatButton.setOnClickListener {
+            val profileUpdates = userProfileChangeRequest {  }
+
+            user!!.updateProfile(profileUpdates).addOnCompleteListener { task ->
+                if(task.isSuccessful) {
+                    if(user.isEmailVerified) {
+                        reload()
+                    } else {
+                        Toast.makeText(this, "Por favor verifica tu correo",
+                            Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
+
+        binding.signOutImageView.setOnClickListener {
+            signOut()
+        }
+
     }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if(currentUser != null){
+            if(currentUser.isEmailVerified) {
+                reload()
+            } else {
+                sendEmailVerification()
+            }
+        }
+    }
+
+    private fun sendEmailVerification() {
+        val user = auth.currentUser
+        user!!.sendEmailVerification().addOnCompleteListener(this) { task ->
+            if (task.isSuccessful) {
+                Toast.makeText(this, "Se envio un correo de verificación",
+                    Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun reload() {
+        val intent = Intent (this, MainActivity::class.java)
+        this.startActivity(intent)
+    }
+
+    private fun signOut() {
+        Firebase.auth.signOut()
+        val intent = Intent(this, SignUpActivity::class.java)
+        startActivity(intent)
+    }
+
 }
--- app/src/main/java/dev/deiby/esteban/appbook_firebase/MainActivity.kt
+++ app/src/main/java/dev/deiby/esteban/appbook_firebase/MainActivity.kt
@@ -1,11 +1,33 @@
 package dev.deiby.esteban.appbook_firebase
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import dev.deiby.esteban.appbook_firebase.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        auth = Firebase.auth
+
+        binding.button.setOnClickListener{
+            signOut()
+        }
     }
+
+    private fun signOut() {
+        Firebase.auth.signOut()
+        val intent = Intent(this, SignUpActivity::class.java)
+        startActivity(intent)
+    }
+
 }
