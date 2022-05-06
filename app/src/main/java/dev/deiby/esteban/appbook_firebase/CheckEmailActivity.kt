a1c304121256bdc1c0545c4a3b33454d90dba00b
DeibyRayo <deibyrayo95@gmail.com>
2022-05-05 22:04:14 -0500

    login-register-checkEmail

---
+++ app/src/main/java/dev/deiby/esteban/appbook_firebase/CheckEmailActivity.kt
@@ -0,0 +1,11 @@
+package dev.deiby.esteban.appbook_firebase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CheckEmailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_check_email)
+    }
+}
